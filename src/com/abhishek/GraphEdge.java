1c91cb2aac9629fac7b974ef8af2c1b331d2b774
Abhishek Dey Das <abhishek@abhis.ws>
2015-02-09 21:54:29 -0600

    AP-3 Used Djikstras

---
+++ src/com/abhishek/GraphEdge.java
@@ -0,0 +1,11 @@
+package com.abhishek;
+
+/**
+ * Created by abhishek on 2/9/15.
+ * Website: adeydas.com
+ */
+public class GraphEdge {
+    public GraphNode start;
+    public GraphNode end;
+    public int edgeWeight;
+}
---
+++ src/com/abhishek/GraphNode.java
@@ -0,0 +1,11 @@
+package com.abhishek;
+
+/**
+ * Created by abhishek on 2/9/15.
+ * Website: adeydas.com
+ */
+public class GraphNode {
+
+    public int value;
+    public GraphEdge[] edges;
+}
