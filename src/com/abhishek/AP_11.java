18735580a70159ba6766ecfa225c14ef2fc66464
Abhishek Dey Das <abhishek.deydas@tangomc.com>
2015-02-11 16:33:25 -0600

    AP-23 #comment Added algorithm with time complexity O(lgn) in average case and O(n) in worst case

--- src/com/abhishek/AP_11.java
+++ src/com/abhishek/AP_11.java
@@ -34,23 +34,30 @@ public class AP_11 {
     }
 
     public static void main(String... args) {
-        Node node1 = new Node();
-        node1.value = (Object)1;
-        node1.left = null;
-        node1.right = null;
+//        Node node1 = new Node();
+//        node1.value = (Object)1;
+//        node1.left = null;
+//        node1.right = null;
+//
+//        Node node2 = new Node();
+//        node2.value = (Object)2;
+//        node2.left = null;
+//        node2.right = null;
+//
+//        Node root = new Node();
+//        root.value = (Object)3;
+//        root.left = node1;
+//        root.right = node2;
 
-        Node node2 = new Node();
-        node2.value = (Object)2;
-        node2.left = null;
-        node2.right = null;
+        Node four = new Node(4, null, null);
+        Node five = new Node(5, null, null);
+        Node seven = new Node(7, null, null);
+        Node two = new Node(2, four, five);
+        Node three = new Node(3, null, seven);
+        Node one = new Node(1, two, three);
 
-        Node root = new Node();
-        root.value = (Object)3;
-        root.left = node1;
-        root.right = node2;
-
-        new AP_11().preOrderRecurse(root);
-        new AP_11().preOrderIterative(root);
+        new AP_11().preOrderRecurse(one);
+        new AP_11().preOrderIterative(one);
     }
 
 }
