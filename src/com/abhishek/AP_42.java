62ff20e3d87020e3cf40d596df5b118b2109b8c4
Abhishek Dey Das <abhishek@abhis.ws>
2015-02-23 20:40:32 -0600

    AP-42 #comment Added code using Manachers algorithm. Linear time solution. http://abhis.ws/x/IoAb

--- src/com/abhishek/AP_42.java
+++ src/com/abhishek/AP_42.java
@@ -44,11 +44,72 @@ public class AP_42 {
         }
     }
 
+
+    /*
+    Start code using Manacher's algorithm
+     */
+
+    /**
+     * Add '*' to input string
+     * @param string
+     * @return
+     */
+    public String preprocess(String string) {
+        StringBuffer append = new StringBuffer();
+        append.append("*");
+        for (int i=0 ; i < string.length() ; i++) {
+            append.append(string.charAt(i));
+            append.append("*");
+        }
+        return append.toString();
+    }
+
+    public String longestPalindrome(String string) {
+        String s = preprocess(string);
+        char[] T = s.toCharArray();
+        int n = T.length;
+        int[] LPS = new int[n];
+        int centerPos = 0, centerRightPos = 0;
+
+        for (int i=1; i<n-1; i++) {
+            int i_mirror = 2*centerPos-i;
+
+            LPS[i] = (centerRightPos > i) ? Math.min(centerRightPos - i, LPS[i_mirror]) : 0;
+
+            while(T[i + 1 + LPS[i]] == T[i - 1 - LPS[i]])
+                LPS[i]++;
+
+            if (i + LPS[i] > centerRightPos) {
+                centerPos = i;
+                centerRightPos = i + LPS[i];
+            }
+        }
+
+        int maxlen = 0;
+        int centerIndex = 0;
+        for (int i=1; i<n-1; i++) {
+            if (LPS[i] > maxlen) {
+                maxlen = LPS[i];
+                centerIndex = i;
+            }
+        }
+
+        return string.substring((centerIndex - 1 - maxlen)/2, maxlen);
+    }
+    /*
+    End code using Manacher's algorithm
+     */
+
+
+
     public static void main(String... args) {
         String str = "RACECAR";
 
         AP_42 ap_42 = new AP_42();
         System.out.println(ap_42.findLargestPalindrome(str));
+
+        String preprocess = ap_42.longestPalindrome(str);
+        System.out.println("Manacher's : " + preprocess);
     }
 
 }
