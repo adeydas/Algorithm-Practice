f62bb8443f8a58d9b86258de5d514b69e1215d2f
Abhishek Dey Das <abhishek.deydas@tangomc.com>
2015-05-15 14:10:35 -0500

    Morris Traversal

---
+++ src/com/abhishek/TreeNode.java
@@ -0,0 +1,11 @@
+package com.abhishek;
+
+/**
+ * Tree node following Leetcode's DS.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
