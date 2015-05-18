b742e824666c51fc1c99f3a13c63ff8f59ade8df
Abhishek Dey Das <abhishek.deydas@tangomc.com>
2015-05-18 11:11:58 -0500

    Tree node

---
+++ src/com/abhishek/TreeNode.java
@@ -0,0 +1,11 @@
+package com.abhishek;
+
+/**
+ * Tree node following Leetcode's DS.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
