10bdb613efb196a812857d64e602a95bc17bc1c0
Abhishek Dey Das <abhishek@abhis.ws>
2015-06-02 21:30:02 -0500

    3 sum and 4 sum

--- src/com/abhishek/leetcode/ThreeSum.java
+++ src/com/abhishek/leetcode/ThreeSum.java
@@ -3,7 +3,16 @@ package com.abhishek.leetcode;
 import java.util.*;
 
 /**
- * https://leetcode.com/problems/3sum/
+ Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+ Note:
+ Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+ The solution set must not contain duplicate triplets.
+ For example, given array S = {-1 0 1 2 -1 -4},
+
+ A solution set is:
+ (-1, 0, 1)
+ (-1, -1, 2)
  */
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -11,22 +20,23 @@ public class ThreeSum {
             return new ArrayList<>();
 
         Arrays.sort(nums);
-        List<List<Integer>> ret = new ArrayList<>();
-        Set<List<Integer>> check = new HashSet<>();
 
-        for (int i=0; i<nums.length; i++) {
+        List<List<Integer>> allTriplets = new ArrayList<>();
+
+        for (int i=0; i<nums.length-2; i++) {
+
+            if (i>0 && nums[i]==nums[i-1]) continue;
+
             int l = i+1;
             int r = nums.length-1;
 
-            while (l<r) {
+            while (l < r) {
                 if (nums[i] + nums[l] + nums[r] == 0) {
-                    List<Integer> l1 = new ArrayList<>();
-                    l1.add(nums[i]); l1.add(nums[l]); l1.add(nums[r]);
-                    if (!check.contains(l1)) {
-                        ret.add(l1);
-                        check.add(l1);
-                    }
-                    l++; r--;
+                    ArrayList t = new ArrayList();
+                    t.add(nums[i]); t.add(nums[l]); t.add(nums[r]);
+                    allTriplets.add(t);
+                    do { l++; } while (l<r && nums[l] == nums[l-1]);
+                    do { r--; } while (l<r && nums[r] == nums[r+1]);
                 } else if (nums[i] + nums[l] + nums[r] < 0) {
                     l++;
                 } else {
@@ -34,8 +44,7 @@ public class ThreeSum {
                 }
             }
         }
-
-        return ret;
+        return allTriplets;
     }
 
     public static void main(String... args) {
